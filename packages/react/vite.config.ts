120599a49868972f84c0f0ecc92bc4bacf650c0c
wolfgang <wolfgang@lemon3.at>
2025-03-01 11:11:38 +0100

    vitest file

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -42,12 +42,6 @@ export default defineConfig({
       ],
     },
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: false,
-  },
 
   resolve: {
     alias: [
